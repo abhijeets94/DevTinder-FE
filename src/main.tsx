a28b43cd4febc245f672e09f0dacda9b8d832340
Abhijeet Singh <abhijeets.94gmail.com@Abhijeets-MacBook-Air.local>
2026-03-29 01:40:14 +0530

    added routings

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Profile } from "./Profile.tsx";
+import { Login } from "./Login.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/profile" element={<Profile/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);

50d7ff434a5aae13202c0e2eb6861ad2dc168530
Abhijeet Singh <abhijeets.94gmail.com@Abhijeets-MacBook-Air.local>
2026-04-07 16:39:37 +0530

    update BASE_URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-// export const BASE_URL = "/api";
-export const BASE_URL = "http://localhost:7777";
+export const BASE_URL = "/api";
+// export const BASE_URL = "http://localhost:7777";
 
 export interface User 
   {
